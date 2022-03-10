0facbf869df63b5cb091cac3ea3adcdf830afe6a
Junaid Iqbal <junaidiqbal0323@gmail.com>
2022-03-10 13:38:12 +0500

    why choose us page updated

--- dna/settings.py
+++ dna/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-_pr-%k^qholmm%+9@j4!rq7kj=(70j90%a0yn4^=rt=+a4%9la
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
