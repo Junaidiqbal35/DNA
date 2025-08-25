efef968e2cb0ac05c1b6b468ea8e2bcb5fa1bfaf
ZahraAmtul <rudegal111@gmail.com>
2025-08-25 20:35:54 +0500

    add pages

--- order/urls.py
+++ order/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
   path('', TemplateView.as_view(template_name="cart/order_checkout.html"), name='order_checkout'),
+  
+  path('place_order/', TemplateView.as_view(template_name="cart/place_order.html"), name='place_order'),
 ]
